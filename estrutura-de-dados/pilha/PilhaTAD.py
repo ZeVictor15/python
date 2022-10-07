73aaaf47115e5a70668218532c51773170a3f770
José victor andrade oliveira <victorandrade150@gmail.com>
2022-10-07 12:27:19 -0300

    Update PilhaTAD.py

--- estrutura-de-dados/pilha/PilhaTAD.py
+++ estrutura-de-dados/pilha/PilhaTAD.py
@@ -1,5 +1,5 @@
 class Nodo:
-    """Esta classe representa um nodo de uma estrutura encadeada."""
+
     def __init__(self, dado=0, nodo_anterior=None):
         self.dado = dado
         self.anterior = nodo_anterior
@@ -8,51 +8,69 @@ class Nodo:
         return '%s <- %s' % (self.anterior, self.dado)
 
 class Pilha:
-    """Esta classe representa uma pilha usando uma estrutura encadeada."""
     
     def __init__(self):
         self.topo = None
-        self.tamanho=0
+        self._tamanho=0
         
     def __repr__(self):
         return "[" + str(self.topo) + "]"
     
+    def __len__(self):
+        return self._tamanho
+    
     def vazia(self):
-        """Retorna True se a pilha está vazia e False caso contrário."""
+        
         if(self.topo==None):
             return True
         else:
             return False
         
     def insere(self, novo_dado):
-        """Insere um elemento no final da pilha."""
-        # Cria um novo nodo com o dado a ser armazenado.
         novo_nodo = Nodo(novo_dado)
-    
-        # Faz com que o novo nodo seja o topo da pilha.
+        
         novo_nodo.anterior = self.topo
         
-        # Faz com que a cabeça da lista referencie o novo nodo.
         self.topo = novo_nodo
-        self.tamanho+=1
+        self._tamanho+=1
         
     def remove(self):
-        """Remove o elemento que está no topo da pilha."""
         
         if(self.vazia()):
             print("Impossível remover valor de pilha vazia.")
         else:
-            self.tamanho-=1
+            self._tamanho-=1
             removido=self.topo.dado
             self.topo = self.topo.anterior
             return removido
     
-    def compara_pilha(pilha):
-        pass
-    
-stack = Pilha()
-stack.insere(5)
-stack.insere(23)
-stack.insere(32)
-print(stack.tamanho)
-print(stack)
+    def compara_pilha(self,pilha1,pilha2):
+        flag = True
+        
+        if len(pilha1) != len(pilha2):
+            flag = False
+            return flag
+            
+        while len(pilha1):
+            if self.pilha1.topo == self.pilha2.topo:
+                pilha1.remove()
+                pilha2.remove()
+            else:
+                flag = False
+                break
+
+stack1 = Pilha()
+stack2 = Pilha()
+stack1.insere(5)
+stack1.insere(23)
+stack1.insere(32)
+
+stack2.insere(5)
+stack2.insere(23)
+stack2.insere(32)
+
+compara = Pilha()
+
+compara.compara_pilha(stack1,stack2)
+
+
